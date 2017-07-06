e7f81012f206f1ac086b8755b839bd421276da21
Nikunj Kanetiya <kanetiya@gmail.com>
2017-07-06 18:36:16 +0530

    Create basic structure & service provide for laravel package

---
+++ src/ColorPaletteServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace NikKanetiya\LaravelColorPalette;
+
+use Illuminate\Support\ServiceProvider;
+
+class ColorPaletteServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
